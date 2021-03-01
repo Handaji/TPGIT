decd7a50e06df9744afa188f48835d9d31f0a5cc
Handaji <Handaji@users.noreply.github.com>
2021-03-01 10:20:55 +0100

    NEW BRANCH AND NEW CLASS

--- src/testgit/testingcalss.java
+++ src/testgit/testingcalss.java
@@ -1,12 +1,10 @@
 package testgit;
 
-public class Main {
+public class testingcalss {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0 ;i<7;i++)
-		System.out.println("hello world");
-		//comment test
+
 	}
 
 }
